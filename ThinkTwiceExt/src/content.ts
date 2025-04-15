572760fa1dfe3e6a30c209d9e77341747e0e999f
MeowB <meolearn@gmail.com>
2025-04-15 11:58:52 +0200

    setup ext

---
+++ ThinkTwiceExt/src/content.ts
@@ -0,0 +1,1 @@
+console.log('ThinkTwice Content Script Loaded');
--- ThinkTwiceExt/vite.config.ts
+++ ThinkTwiceExt/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		outDir: 'dist', // Default output folder for build
+		rollupOptions: {
+			input: {
+				popup: path.resolve(__dirname, 'index.html'), // Popup entry
+				background: path.resolve(__dirname, 'src/background.ts'), // Background script
+				content: path.resolve(__dirname, 'src/content.ts'), // content script
+			},
+			output: {
+			  // Use specific names for chunk files without hashing
+			  entryFileNames: 'assets/[name].js',
+			  chunkFileNames: 'assets/[name].js',
+			  assetFileNames: 'assets/[name][extname]', // Keep original asset names for images and other assets
+			},
+		},
+
+		emptyOutDir: true,
+	}
+});
