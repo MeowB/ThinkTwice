da01368dca7e9b4e8049f8c2793776f92efa968b
Scott Borlon <60074375+MeowB@users.noreply.github.com>
2025-04-16 10:06:56 +0200

    plugin on load

--- ThinkTwiceExt/src/content.ts
+++ ThinkTwiceExt/src/content.ts
@@ -3,7 +3,34 @@ import getReadableText from './functions/getReadableText'
 
 console.log('ThinkTwice Content Script Loaded');
 
-const readable = getReadableText();
-console.log('text: ', readable);
 
-// TODO: scan 
+function waitForElement(selector: string, callback: Function) {
+	const seen = new Set<HTMLElement>()
+
+	const checkForNewElements = () => {
+		const elements = document.querySelectorAll(selector)
+
+		elements.forEach((el: any) => {
+			if (!seen.has(el)) {
+				seen.add(el);
+				callback(el)
+			}
+		});
+	}
+
+	// initial check 
+	checkForNewElements();
+
+	const observer = new MutationObserver(() => {
+		checkForNewElements()
+	})
+
+	observer.observe(document.body, { childList: true, subtree: true })
+}
+
+waitForElement("article, section, main, p, h1, h2, h3, h4, h5, h6, li, blockquote span", () => {
+	const readable = getReadableText();
+	if (readable.length > 0) {
+		console.log('text: ', readable);
+	}
+})
--- ThinkTwiceExt/src/functions/getReadableText.ts
+++ ThinkTwiceExt/src/functions/getReadableText.ts
@@ -1,24 +1,31 @@
 export default function getReadableText() {
-	// Create a variable to store all readable text
-	let textContent = '';
-
-	// Get all elements in the body of the page
-	const selectors = "article, section, main, p, h1, h2, h3, h4, h5, h6, li, blockquote";
+	const selectors = "article, section, main, p, h1, h2, h3, h4, h5, h6, li, blockquote span";
 	const elements = document.querySelectorAll(selectors);
+	const seenAttr = 'data-read-processed'
+
+	let textContent: string[] = []
 
 	// Loop through each element and check if it contains text
 	elements.forEach(element => {
+		// skip if already processed
+		if (element.hasAttribute(seenAttr)) return;
+		if (element.tagName === 'SCRIPT' || element.tagName === 'STYLE') return;
+
 		// Skip non-readable elements like scripts, styles, etc.
-		if (element.tagName !== 'SCRIPT' && element.tagName !== 'STYLE') {
-			// Add the text content of each element to the textContent variable
-			if (element instanceof HTMLElement) {
-				textContent += element.innerText || element.textContent;
-			} else {
-				textContent += element.textContent || ''
-			}
+		// Add the text content of each element to the textContent variable
+		let content = <string>''
+		if (element instanceof HTMLElement) {
+			content += element.innerText || element.textContent;
+		} else {
+			content += element.textContent || ''
+		}
+
+		if (content.trim() !== '') {
+			textContent.push(content.trim())
+			element.setAttribute(seenAttr, 'true')
 		}
 	});
 
-	return textContent.trim().split('\n').filter((item: string) => item !== '').filter(line => !/^\s*$/.test(line)); // Return the accumulated text content
+	return textContent
 }
 
